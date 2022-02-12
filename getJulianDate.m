d40cc7dbb00f433ca91366116230c3b77cac3583
Rohan Patel <56667817+rohanpatel747@users.noreply.github.com>
2022-02-12 12:33:16 -0700

    IC Julian Date Calc from MATLAB (Wrapper)

---
+++ getJulianDate.m
@@ -0,0 +1,14 @@
+function jd = getJulianDate(d)
+%LAMBERTNREV Get Julian Date using built-in MATLAB function
+%
+%   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+%   Inputs:
+%       1. d [string] Date Character format: 'dd-Mmm-yyyy'
+%   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+%   Output: out structure containing fields:
+%       1. jd  [1x1]  Julian Date from MATLAB
+%   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
+%
+jd = juliandate(d,'dd-mm-yyyy');
+end
+
