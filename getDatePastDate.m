0cb8807e6574c6fb730c8cb90eccd5c55ae21273
Rohan Patel <56667817+rohanpatel747@users.noreply.github.com>
2022-02-16 08:00:05 -0700

    [IC] getDatePastDays - function to find the calendar date after N number of days from another cal date

--- getDatePastDate.m
+++ getDatePastDate.m
@@ -1,14 +1,17 @@
-function jd = getJulianDate(d)
-%GETJULIANDATE Get Julian Date using built-in MATLAB function
+function D = getDatePastDate(d,t)
+%GETDATEPASTDATE Get Julian Date using built-in MATLAB function
 %
 %   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 %   Inputs:
 %       1. d [string] Date Character format: 'dd-mm-yyyy'
+%       2. t [1x1]    Number of Days Past the Date
 %   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 %   Output: out structure containing fields:
-%       1. jd  [1x1]  Julian Date from MATLAB
+%       1. D  [1x1]  Julian Date from MATLAB
 %   - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - - -
 %
-jd = juliandate(d,'dd-mm-yyyy');
+    jd = juliandate(d,'dd-mm-yyyy');
+    jdn= jd+t;
+    D = datetime(jdn,'convertfrom','juliandate','Format','dd-MM-yyyy');
 end
 
